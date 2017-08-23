daebfa503b69ab43515252c1003c9355e72ff222
Amaldevks <31182927+Amaldevks@users.noreply.github.com>
2017-08-23 16:49:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,8 +36,8 @@ submit.onclick = function(){
     //capture a list of names and render it as alist
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for (var i=0; i<names.length; i++){
-        list+-'<li>' + names[i]+'</li>';
+    for (var i=0; i< names.length; i++){
+        list+='<li>' + names[i]+'</li>';
         
     }
     
